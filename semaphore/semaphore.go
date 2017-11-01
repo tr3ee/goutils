8e8bc50d224a380f9be23b390f14a3f119323499
tr3ee <807596654@qq.com>
2017-11-01 11:34:54 +0800

    semaphore: normalize the code without changing the interface

--- semaphore/semaphore.go
+++ semaphore/semaphore.go
@@ -2,28 +2,33 @@ package semaphore
 
 import "sync"
 
+// Semaphore implements the signal mechanism management
 type Semaphore struct {
 	Threads chan int
-	Wg sync.WaitGroup
+	Wg      sync.WaitGroup
 }
 
+// NewSemaphore returns a new semaphore
 func NewSemaphore(n int) *Semaphore {
 	inst := new(Semaphore)
-	inst.Threads = make(chan int,n)
+	inst.Threads = make(chan int, n)
 	// inst.Wg=sync.WaitGroup{}
 	return inst
 }
 
-func (self *Semaphore) P() {
-	self.Threads <- 1
-	self.Wg.Add(1)
+// P is a primitive operation that requests the allocation of a unit resource
+func (sem *Semaphore) P() {
+	sem.Threads <- 1
+	sem.Wg.Add(1)
 }
 
-func (self *Semaphore) V() {
-	self.Wg.Done()
-	<-self.Threads
+// V is a primitive operation that releases a unit of resources
+func (sem *Semaphore) V() {
+	sem.Wg.Done()
+	<-sem.Threads
 }
 
-func (self *Semaphore) Wait() {
-	self.Wg.Wait()
+// Wait waits for all unit resources to be released
+func (sem *Semaphore) Wait() {
+	sem.Wg.Wait()
 }
